a78aeb807346b1d7e617615bc5945dad324640c7
Jacob Rief <jacob.rief@gmail.com>
2020-05-18 18:14:17 +0200

    remember to upgrade classifiers and install_requirements

--- filer/__init__.py
+++ filer/__init__.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-See PEP 386 (https://www.python.org/dev/peps/pep-0386/)
+See PEP 440 (https://www.python.org/dev/peps/pep-0440/)
 
 Release logic:
  1. Increase version number (change __version__ below).
- 2. Check that all changes have been documented in CHANGELOG.rst.
- 3. git add filer/__init__.py CHANGELOG.rst
- 4. git commit -m 'Bump to {new version}'
- 5. git push
- 6. Assure that all tests pass on https://travis-ci.org/github/divio/django-filer.
- 7. git tag {new version}
- 8. git push --tags
- 9. python setup.py sdist
-10. twine upload dist/django-filer-{new version}.tar.gz
+ 2. Assure that all changes have been documented in CHANGELOG.rst.
+ 3. In setup.py check that
+   - versions from all third party packages are pinned in ``REQUIREMENTS``.
+   - the list of ``CLASSIFIERS`` is up to date.
+ 4. git add filer/__init__.py CHANGELOG.rst setup.py
+ 5. git commit -m 'Bump to {new version}'
+ 6. git push
+ 7. Assure that all tests pass on https://travis-ci.org/github/divio/django-filer.
+ 8. git tag {new_version_number}
+ 9. git push --tags
+10. python setup.py sdist
+11. twine upload dist/django-filer-{new_version_number}.tar.gz
 """
 
 __version__ = '1.7.1'
