e76ab89f4ea17926088e33b61d625a84f34ffbd0
Jacob Rief <jacob.rief@uibk.ac.at>
2022-04-07 14:05:21 +0200

    update 3rd party requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from filer import __version__
 REQUIREMENTS = [
     'django>=3.2,<4.1',
     'django-mptt>=0.6,<1.0',  # the exact version depends on Django
-    'django-polymorphic>=2,<3.1',
+    'django-polymorphic>=3',
     'easy-thumbnails>=2.8.0',
     'Unidecode>=0.04,<1.2',
 ]
