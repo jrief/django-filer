d5068399ec41149ecdcdb51cce3e83f5107b312c
Jacob Rief <jacob.rief@gmail.com>
2023-07-31 09:52:01 +0200

    WiP: Better UX for filer-admin

--- client/FolderAdmin.tsx
+++ client/FolderAdmin.tsx
@@ -1,30 +1,82 @@
 import {useState} from 'react';
-import {DndContext} from '@dnd-kit/core';
-import {arrayMove, SortableContext} from '@dnd-kit/sortable';
-import {Droppable} from './Droppable';
-import {Draggable} from './Draggable';
-import {SortableItem} from "./SortableItem";
+import {DndContext, useDraggable, useDroppable} from '@dnd-kit/core';
 
+
+function Draggable(props) {
+	const { children, id } = props;
+	const {attributes, listeners, setNodeRef, transform} = useDraggable({
+		id: id,
+	});
+	const style = {
+		border: '1px solid blue',
+		height: '40px',
+		width: '80px',
+	};
+	if (transform) {
+		style['transform'] = `translate(${transform.x}px, ${transform.y}px)`;
+	}
+
+	return (
+		<div ref={setNodeRef} style={style} {...listeners} {...attributes}>
+			{children}
+		</div>
+	);
+}
+
+
+function Droppable(props) {
+	const { children, id } = props;
+	const {attributes, listeners, transform} = useDraggable({
+		id: id,
+	});
+	const {isOver, setNodeRef} = useDroppable({
+		id: id,
+	});
+	const style = {
+		border: '1px solid grey',
+		color: isOver ? 'green' : undefined,
+		height: '40px',
+		width: '120px',
+		marginTop: '10px',
+	};
+	if (transform) {
+		style['transform'] = `translate(${transform.x}px, ${transform.y}px)`;
+	}
+
+	return (
+		<div ref={setNodeRef} style={style} {...listeners} {...attributes}>
+			{children}
+		</div>
+	);
+}
 
 export default function FolderAdmin() {
-	const [languages, setLanguages] = useState(['JavaScript', 'Python', 'TypeScript']);
+	const containers = ['A', 'B', 'C'];
+	const [parent, setParent] = useState(null);
+	const draggableMarkup = (
+		<Draggable id="0">Drag me</Draggable>
+	);
 
 	return (
 		<DndContext onDragEnd={handleDragEnd}>
-			<SortableContext items={languages}>
-			{languages.map(language => <SortableItem key={language} id={language}/>)}
-			</SortableContext>
+			{parent === null ? draggableMarkup : null}
+
+			{containers.map((id) => (
+				// We updated the Droppable component so it would accept an `id`
+				// prop and pass it to `useDroppable`
+				<Droppable key={id} id={id}>
+				  {parent === id ? draggableMarkup : `Drop over ${id}`}
+				</Droppable>
+			))}
 		</DndContext>
 	);
 
 	function handleDragEnd(event) {
 		const {active, over} = event;
-		if (active.id !== over.id) {
-			setLanguages(items => {
-				const activeIndex = items.indexOf(active.id);
-				const overIndex = items.indexOf(over.id);
-				return arrayMove(items, activeIndex, overIndex);
-			});
-		}
+		// debugger;
+
+		// If the item is dropped over a container, set it as the parent
+		// otherwise reset the parent to `null`
+		setParent(over ? over.id : null);
 	}
 }
