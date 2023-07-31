6c06959716c61607e6221ac77e0dbc8e0be0e140
Jacob Rief <jacob.rief@gmail.com>
2023-07-31 15:49:58 +0200

    MVP: dragging files and folders working

--- client/FolderAdmin.tsx
+++ client/FolderAdmin.tsx
@@ -1,21 +1,27 @@
 import {useState} from 'react';
-import {DndContext, useDraggable, useDroppable} from '@dnd-kit/core';
+import {DndContext, pointerWithin, useDraggable, useDroppable} from '@dnd-kit/core';
 
 
 function Draggable(props) {
 	const { children, id } = props;
-	const {attributes, listeners, setNodeRef, transform} = useDraggable({
+	const {
+		attributes,
+		listeners,
+		setNodeRef,
+		transform
+	} = useDraggable({
 		id: id,
+		data: {foo: 'bar'},
 	});
 	const style = {
 		border: '1px solid blue',
 		height: '40px',
-		width: '80px',
+		width: '120px',
+		marginTop: '10px',
 	};
 	if (transform) {
 		style['transform'] = `translate(${transform.x}px, ${transform.y}px)`;
 	}
-
 	return (
 		<div ref={setNodeRef} style={style} {...listeners} {...attributes}>
 			{children}
@@ -26,57 +32,68 @@ function Draggable(props) {
 
 function Droppable(props) {
 	const { children, id } = props;
-	const {attributes, listeners, transform} = useDraggable({
+	const {
+		isOver,
+		active,
+		setNodeRef
+	} = useDroppable({
 		id: id,
 	});
-	const {isOver, setNodeRef} = useDroppable({
-		id: id,
-	});
+	const validTarget = isOver && active.id !== id;
 	const style = {
-		border: '1px solid grey',
-		color: isOver ? 'green' : undefined,
-		height: '40px',
-		width: '120px',
-		marginTop: '10px',
+		backgroundColor: validTarget ? 'green' : undefined,
+		height: '100%',
+		width: '100%',
 	};
-	if (transform) {
-		style['transform'] = `translate(${transform.x}px, ${transform.y}px)`;
-	}
-
 	return (
-		<div ref={setNodeRef} style={style} {...listeners} {...attributes}>
+		<div ref={setNodeRef} style={style}>
 			{children}
 		</div>
 	);
 }
 
-export default function FolderAdmin() {
-	const containers = ['A', 'B', 'C'];
-	const [parent, setParent] = useState(null);
-	const draggableMarkup = (
-		<Draggable id="0">Drag me</Draggable>
-	);
+function Folder(props) {
+	const { children, id } = props;
 
 	return (
-		<DndContext onDragEnd={handleDragEnd}>
-			{parent === null ? draggableMarkup : null}
+		<Draggable id={id}>
+			<Droppable id={id}>
+				{id}
+			</Droppable>
+		</Draggable>
+	);
+}
 
-			{containers.map((id) => (
+
+export default function FolderAdmin() {
+	const [files, setFiles] = useState(['a', 'b']);
+	const [folders, setFolders] = useState(['A', 'B', 'C']);
+
+	return (
+		<DndContext onDragStart={handleDragStart} onDragEnd={handleDragEnd} collisionDetection={pointerWithin}>
+			{folders.map((id) => (
 				// We updated the Droppable component so it would accept an `id`
 				// prop and pass it to `useDroppable`
-				<Droppable key={id} id={id}>
-				  {parent === id ? draggableMarkup : `Drop over ${id}`}
-				</Droppable>
+				<Folder key={id} id={id} />
+			))}
+			{files.map((id) => (
+				<Draggable key={id} id={id}>{id}</Draggable>
 			))}
 		</DndContext>
 	);
 
+	function handleDragStart(event) {
+		const {active} = event;
+		console.log(active);
+	}
+
 	function handleDragEnd(event) {
 		const {active, over} = event;
-		// debugger;
+		console.log(active, over);
 
-		// If the item is dropped over a container, set it as the parent
-		// otherwise reset the parent to `null`
-		setParent(over ? over.id : null);
+		if (over && active.id !== over.id) {
+			setFolders(folders.filter(f => f !== active.id));
+			setFiles(files.filter(f => f !== active.id));
+		}
 	}
 }
